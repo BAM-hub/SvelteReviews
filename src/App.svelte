ef5ea6917d83778f4cc67b10aab08209bebf2ce9
BAM-hub <bsharamin12@gmail.com>
2022-06-02 15:03:30 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
+  import FeedbackFrom from "./components/FeedbackForm.svelte";
+</script>
+
+<main class="container">
+  <FeedbackFrom />
+  <FeedbackStats />
+  <FeedbackList />
+</main>
